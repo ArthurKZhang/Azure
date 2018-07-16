debe89fe42d7323720dbe189fe01eba15f0159c5
ArthurKZhang <arthurzhang73@gmail.com>
2018-07-16 17:32:27 +0800

    [Logging Annotation] 1. Method and Constructor: parameters&returns&timeConsuming; 2. Annotation priority: methods or fields' annotation hold the higher priority; 3. Logging the info of fields when getting or setting fields' value.

---
+++ app/src/main/java/com/arthur/azure/A.java
@@ -0,0 +1,14 @@
+package com.arthur.azure;
+
+import com.arthur.azure_core.annotations.Logging;
+
+//@Logging(tag = "in A Class")
+public class A {
+    @Override
+    public String toString() {
+        return "A{}";
+    }
+
+    public A() {
+    }
+}
